08796b53df19ac79efb20f11fc06021b8097e2a8
Feross Aboukhadijeh <feross@feross.org>
2017-08-23 00:30:12 -0700

    Move render router to app.js so the error route actually catches errors

--- src/server/app.js
+++ src/server/app.js
@@ -2,19 +2,23 @@ module.exports = {
   init
 }
 
+const { h } = require('preact') /** @jsx h */
+
 const compress = require('compression')
 const crypto = require('crypto')
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const Provider = require('preact-context-provider')
 const session = require('express-session')
 
+const App = require('../views/app')
 const config = require('../../config')
-const secret = require('../../secret')
-
+const createRenderer = require('../lib/preact-dom-renderer')
+const createStore = require('../store')
 const routerApi = require('./router-api')
 const routerLogin = require('./router-login')
-const routerRender = require('./router-render')
+const secret = require('../../secret')
 
 function init (sessionStore) {
   const app = express()
@@ -129,7 +133,62 @@ function init (sessionStore) {
 
   app.use('/api', routerApi)
   app.use('/auth', routerLogin)
-  app.use(routerRender)
+
+  // Render all routes on the server
+  app.get('*', (req, res) => handleRender(null, req, res))
+
+  // Log errors to Opbeat
+  if (global.opbeat) app.use(global.opbeat.middleware.express())
+
+  // Handle errors with the same server-side rendering path
+  app.use((err, req, res, next) => handleRender(err, req, res))
+
+  function handleRender (err, req, res) {
+    const renderer = createRenderer()
+    const { store, dispatch } = createStore(update, onFetchEnd)
+
+    if (err) {
+      console.error(err.stack)
+      store.errors.push(err.message)
+    }
+
+    store.userName = (req.session.user && req.session.user.userName) || null
+
+    const jsx = (
+      <Provider store={store} dispatch={dispatch}>
+        <App />
+      </Provider>
+    )
+
+    dispatch('LOCATION_REPLACE', req.url)
+
+    if (store.app.fetchCount === 0) done()
+
+    function onFetchEnd () {
+      if (store.app.fetchCount === 0) process.nextTick(done)
+    }
+
+    function update () {
+      renderer.render(jsx)
+    }
+
+    function done () {
+      const { location, errors } = store
+
+      let status = 200
+
+      if (err) {
+        status = typeof err.status === 'number' ? err.status : 500 // Internal Server Error
+      } else if (location.name === 'not-found' || errors.length > 0) {
+        status = 404
+      }
+
+      res.status(status)
+
+      const content = renderer.html()
+      res.render('index', { content, store, title: store.app.title })
+    }
+  }
 
   return app
 }
