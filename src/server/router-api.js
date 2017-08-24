4b3d1b82133a25c2be24610bc8bc4720d9f5254a
Feross Aboukhadijeh <feross@feross.org>
2017-08-24 14:20:12 -0700

    unify how api route errors are handled

--- src/server/router-api.js
+++ src/server/router-api.js
@@ -4,39 +4,35 @@ const api = require('../api')
 
 const router = express.Router()
 
+function sendError (next, err, status) {
+  err.status = status || 400
+  return next(err)
+}
+
 router.get('/doc/get', (req, res, next) => {
   api.doc.get(req.query, (err, result) => {
-    if (err) {
-      err.status = 404
-      return next(err)
-    }
+    if (err) return sendError(next, err, 404)
     res.json({ result })
   })
 })
 
 router.get('/snippet/get', (req, res, next) => {
   api.snippet.get(req.query, (err, result) => {
-    if (err) {
-      err.status = 404
-      return next(err)
-    }
+    if (err) return sendError(next, err, 404)
     res.json({ result })
   })
 })
 
 router.get('/snippet/all', (req, res, next) => {
   api.snippet.all(req.query, (err, result) => {
-    if (err) {
-      err.status = 404
-      return next(err)
-    }
+    if (err) return sendError(next, err, 404)
     res.json({ result })
   })
 })
 
 router.post('/snippet/add', (req, res, next) => {
   if (!req.session.user) {
-    return next(new Error('Must be logged in to add snippets'))
+    return sendError(next, new Error('Must be logged in to add snippets'))
   }
 
   const opts = Object.assign({}, req.query, {
@@ -44,18 +40,14 @@ router.post('/snippet/add', (req, res, next) => {
   })
 
   api.snippet.add(opts, (err, result) => {
-    if (err) {
-      err.status = 404
-      return next(err)
-    }
+    if (err) return sendError(next, err, 404)
     res.json({ result })
   })
 })
 
 router.get('*', (req, res, next) => {
   const err = new Error('404 Not Found')
-  err.status = 404
-  next(err)
+  sendError(next, err, 404)
 })
 
 if (global.opbeat) router.use(global.opbeat.middleware.express())
