b6000f70a4e26ac0ebdb22bcd2ce8a39c69e10ae
Feross Aboukhadijeh <feross@feross.org>
2017-08-23 18:01:25 -0700

    Fix unstyled flash on load

--- src/views/link.js
+++ src/views/link.js
@@ -3,21 +3,24 @@ const c = require('classnames')
 
 const Link = (props) => {
   const {
+    children,
+    class: className,
     href = '#',
-    onClick = () => {}
+    onClick = () => {},
+    ...rest
   } = props
 
   return (
     <a
-      class={c('link', props.class)}
+      class={c('link', className)}
       href={href}
       onClick={e => {
         if (href === '#') e.preventDefault()
         onClick(e)
       }}
-      style={props.style}
+      {...rest}
     >
-      {props.children}
+      {children}
     </a>
   )
 }
