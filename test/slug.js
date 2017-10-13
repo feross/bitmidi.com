af88ccda02e6832f9b345bf4f9e0c41c9d740295
Feross Aboukhadijeh <feross@feross.org>
2017-10-13 11:48:59 -0700

    style

--- test/slug.js
+++ test/slug.js
@@ -2,37 +2,37 @@ const test = require('tape')
 const slug = require('../src/lib/slug')
 
 test('slug', (t) => {
-  // simple case
+  // Simple case
   t.equal(
     slug('one two three'),
     'one-two-three'
   )
 
-  // too many words
+  // Too many words
   t.equal(
     slug('one two three four five six seven eight nine ten eleven twelve thirteen'),
     'one-two-three-four-five-six-seven-eight-nine-ten-eleven-twelve'
   )
 
-  // too many characters
+  // Too many characters
   t.equal(
     slug(Array(10).join('supercalifragilisticexpialidocious')),
     'supercalifragilisticexpialidocioussupercalifragilisticexpialidocioussuperca'
   )
 
-  // unicode emoji
+  // Unicode emoji
   t.equal(
     slug('so fancy ✨'),
     'so-fancy-sparkles'
   )
 
-  // unicode japanese
+  // Unicode Japanese
   t.equal(
     slug('昨夜のコンサートは最高でした。'),
     'unicode'
   )
 
-  // remove dot character
+  // Remove dot character
   t.equal(
     slug('node.js is cool'),
     'node-js-is-cool'
