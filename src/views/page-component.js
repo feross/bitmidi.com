bbcef2740e69e57f012fd4241f847942ff16a10b
Feross Aboukhadijeh <feross@feross.org>
2017-10-13 19:41:24 -0700

    page component: better detection of changed params

--- src/views/page-component.js
+++ src/views/page-component.js
@@ -8,11 +8,7 @@ class PageComponent extends Component {
   componentWillReceiveProps (nextProps) {
     // If the user navigates, then the location prop of <Page location={location} />
     // will change. Call load() since new data may need to be fetched.
-    if (this.props.location !== nextProps.location) this.load()
-  }
-
-  load () {
-    throw new Error('PageComponent instance is missing required load() method')
+    if (this.props.url !== nextProps.url) this.load()
   }
 }
 
