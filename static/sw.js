78012b63e46cdb04178c4961da795a17fd52e6e3
Feross Aboukhadijeh <feross@feross.org>
2017-10-25 22:20:43 -0700

    cache start url for Lighthouse audit

--- static/sw.js
+++ static/sw.js
@@ -2,47 +2,54 @@
 
 'use strict'
 
-// Incrementing CACHE_VERSION will kick off the install event and force previously cached
-// resources to be cached again.
+// Incrementing CACHE_VERSION will kick off the 'install' event and force previously
+// cached resources to be cached again.
 const CACHE_VERSION = 1
 
 const CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 }
-const OFFLINE_URL = 'offline.html'
+const START_URL = './'
+const OFFLINE_URL = './offline.html'
 
-function createCacheBustedRequest (url) {
+function createCacheBustingRequest (url) {
   const request = new Request(url, { cache: 'reload' })
   // See https://fetch.spec.whatwg.org/#concept-request-mode
-  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see
-  // if the cache: 'reload' option had any effect.
+  // Note: Not supported in Chrome, so check to see if the option had any effect.
   if ('cache' in request) {
     return request
   }
 
-  // If {cache: 'reload'} didn't have any effect, append a cache-busting URL parameter instead.
+  // Append a cache-busting param if {cache: 'reload'} didn't have an effect
   const bustedUrl = new URL(url, self.location.href)
   bustedUrl.search += (bustedUrl.search ? '&' : '') + 'c=' + Date.now()
   return new Request(bustedUrl)
 }
 
+async function addToCache (url) {
+  // We can't use cache.add() here, since we want `url` to be the cache key but
+  // the actual URL we end up requesting might include a cache-busting parameter.
+  const response = await fetch(createCacheBustingRequest(url))
+  if (!response.ok) {
+    throw new Error(`HTTP response error. ${response.status}`)
+  }
+  const cache = await caches.open(CURRENT_CACHES.offline)
+  await cache.put(url, response)
+}
+
 self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
-    // the actual URL we end up requesting might include a cache-busting parameter.
-    const response = await fetch(createCacheBustedRequest(OFFLINE_URL))
-    if (!response.ok) {
-      throw new Error(`HTTP response error. ${response.status}`)
-    }
-    const cache = await caches.open(CURRENT_CACHES.offline)
-    await cache.put(OFFLINE_URL, response)
-  })())
+  // Note: START_URL is never actually served from cache by the service worker. This
+  // exists temporarily to pass the Lighthouse audit.
+  event.waitUntil(Promise.all([
+    addToCache(START_URL),
+    addToCache(OFFLINE_URL)
+  ]))
 })
 
 self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES.
-  // While there is only one cache in this example, the same logic will handle the case where
-  // there are multiple versioned caches.
+  // Delete all caches that aren't named in CURRENT_CACHES. While there is only one
+  // cache in this example, the same logic will handle the case where there are
+  // multiple versioned caches.
   const expectedNames = Object.values(CURRENT_CACHES)
 
   event.waitUntil((async () => {
