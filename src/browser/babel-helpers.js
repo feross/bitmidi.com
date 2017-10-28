83b9125792940411d7bdeb4ba06b9c85f28f05b7
Feross Aboukhadijeh <feross@feross.org>
2017-10-27 23:55:13 -0700

    standard

--- src/browser/babel-helpers.js
+++ src/browser/babel-helpers.js
@@ -1,15 +1,15 @@
 (function (global) {
-  var babelHelpers = global.babelHelpers = {};
+  var babelHelpers = global.babelHelpers = {}
 
   babelHelpers.objectWithoutProperties = function (obj, keys) {
-    var target = {};
+    var target = {}
 
     for (var i in obj) {
-      if (keys.indexOf(i) >= 0) continue;
-      if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
-      target[i] = obj[i];
+      if (keys.indexOf(i) >= 0) continue
+      if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
+      target[i] = obj[i]
     }
 
-    return target;
-  };
-})(typeof global === "undefined" ? self : global);
+    return target
+  }
+})(window)
