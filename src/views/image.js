431d99d446086bb01b3998f2965080f95c4d0aba
Feross Aboukhadijeh <feross@feross.org>
2017-11-03 15:58:48 -0700

    add experimental async attribute to <img>

--- src/views/image.js
+++ src/views/image.js
@@ -7,8 +7,10 @@ const Image = (props) => {
     throw new Error('"props.alt" must be a string of non-zero length')
   }
 
+  // Experimental async attribute, see: https://github.com/whatwg/html/issues/1920
   return (
     <img
+      async
       alt={alt}
       {...rest}
     />
