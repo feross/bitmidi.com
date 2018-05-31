807e90166e2d5abd9ced727a7ff7a4f342cc2f4f
Feross Aboukhadijeh <feross@feross.org>
2018-05-31 01:37:59 -0700

    remove nodefoo SubmitPage and DocPage

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,14 @@
  * List of routes in the app. Specified as [routeName, routePath, routePage].
  */
 
-const DocPage = require('./views/doc-page')
 const ErrorPage = require('./views/error-page')
 const HomePage = require('./views/home-page')
 const SearchPage = require('./views/search-page')
 const SnippetPage = require('./views/snippet-page')
-const SubmitPage = require('./views/submit-page')
 
 module.exports = [
   ['home', '/', HomePage],
-  ['submit', '/submit', SubmitPage],
   ['search', '/search', SearchPage],
-  ['doc', '/docs/:url+', DocPage],
   ['snippet', '/:snippetId', SnippetPage],
   ['error', '(.*)', ErrorPage]
 ]
