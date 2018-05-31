b42ad586b15f0743cae08b74260b32e93525df5c
Feross Aboukhadijeh <feross@feross.org>
2018-05-31 10:10:33 -0700

    log runtime browser errors

--- src/browser/index.mjs
+++ src/browser/index.mjs
@@ -28,6 +28,14 @@ if ('serviceWorker' in navigator) {
     .catch(err => console.error('Unable to register service worker.', err))
 }
 
+// Log runtime browser errors
+window.addEventListener('error', err => {
+  window.gtag('event', 'exception', {
+    'description': err,
+    'fatal': false
+  })
+})
+
 /**
  * DEVELOPMENT
  */
