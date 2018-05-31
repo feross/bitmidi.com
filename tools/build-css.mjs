74e1b84fa776ee585231056ac44c20283a891abe
Feross Aboukhadijeh <feross@feross.org>
2018-05-31 08:35:15 -0700

    build-css: print verbose message

--- tools/build-css.mjs
+++ tools/build-css.mjs
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 
+import fs from 'fs'
+import minimist from 'minimist'
 import path from 'path'
 import purifyCss from 'purify-css'
 import rimraf from 'rimraf'
+import { oneLine } from 'common-tags'
 
 import config from '../config'
 
-const DEBUG = !!process.env.DEBUG
+const argv = minimist(process.argv.slice(2), {
+  boolean: ['verbose']
+})
 
 const outputPath = path.join(config.rootPath, './static/bundle.css')
+
+rimraf.sync(outputPath)
 
 const content = [
   './src/views/*.js',
@@ -26,8 +33,6 @@ const whitelist = [
 ]
 
 const opts = {
-  output: outputPath,
-
   // Selectors to never remove
   whitelist,
 
@@ -35,12 +40,30 @@ const opts = {
   minify: config.isProd,
 
   // Logs stats on how much CSS was removed
-  info: DEBUG,
+  info: argv.debug,
 
   // Logs which CSS rules were removed
-  rejected: false
+  rejected: argv.debug
 }
 
-rimraf.sync(outputPath)
+const startTime = Date.now()
 
-purifyCss(content, css, opts)
+purifyCss(content, css, opts, bundle => {
+  // Write file
+  fs.writeFileSync(outputPath, bundle)
+
+  if (argv.verbose) {
+    const shortOutputPath = path.join(
+      path.basename(path.dirname(outputPath)),
+      path.basename(outputPath)
+    )
+    const buildTime = ((Date.now() - startTime) / 1000).toFixed(2)
+    const currentTime = new Date().toLocaleTimeString()
+
+    // Print success message
+    console.log(oneLine`
+      ${bundle.length} bytes written to ${shortOutputPath}
+      (${buildTime} seconds) at ${currentTime}
+    `)
+  }
+})
