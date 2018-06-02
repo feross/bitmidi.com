8740ccbdd2997522cb61e5ec3acdccc5eeb2a890
Feross Aboukhadijeh <feross@feross.org>
2018-06-02 12:35:16 -0700

    update api router for midi apis

---
+++ src/server/router-api.mjs
@@ -0,0 +1,29 @@
+import Router from 'express-promise-router'
+
+import * as api from '../api'
+
+const router = Router()
+
+router.get('/midi/get', async (req, res) => {
+  res.json({ result: await api.midi.get(req.query) })
+})
+
+router.get('/midi/all', async (req, res) => {
+  res.json({ result: await api.midi.all(req.query) })
+})
+
+router.get('*', async (req, res) => {
+  const err = new Error('404 Not Found')
+  err.status = 404
+  throw err
+})
+
+if (global.opbeat) router.use(global.opbeat.middleware.express())
+
+router.use(async (err, req, res, next) => {
+  const status = typeof err.status === 'number' ? err.status : 400 // Bad Request
+  res.status(status)
+  res.json({ error: { message: err.message, code: err.code } })
+})
+
+export default router
