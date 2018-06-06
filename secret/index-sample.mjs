c18410e78a86e9b2b5ff1ca82a5c960272e75dc4
Feross Aboukhadijeh <feross@feross.org>
2018-06-06 02:21:28 -0700

    ESM secret/index-sample

---
+++ secret/index-sample.mjs
@@ -0,0 +1,17 @@
+exports.cookie = 'TODO'
+
+exports.opbeat = {
+  appId: 'TODO',
+  organizationId: 'TODO',
+  secretToken: 'TODO'
+}
+
+exports.db = {
+  client: 'mysql',
+  connection: {
+    host: 'TODO',
+    user: 'TODO',
+    password: 'TODO',
+    database: 'TODO'
+  }
+}
