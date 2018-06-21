1d2cb1e01d76fb700afa9ff5c934bea0d8474055
Feross Aboukhadijeh <feross@feross.org>
2018-06-21 12:35:36 -0700

    move URL polyfill to server/index.mjs

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -5,6 +5,11 @@ import util from 'util'
 
 import appInit from './app'
 
+// TODO: Remove when upgrading to Node 10
+import nodeUrl from 'url'
+global.URL = nodeUrl.URL
+global.URLSearchParams = nodeUrl.URLSearchParams
+
 export default async function init (port) {
   const app = appInit()
   const server = http.createServer(app)
--- src/server/start.mjs
+++ src/server/start.mjs
@@ -1,8 +1,4 @@
 import init from './index'
 
-// TODO: Remove when upgrading to Node 10 because URL is global
-import nodeUrl from 'url'
-global.URL = nodeUrl.URL
-
 const port = Number(process.argv[2]) || 4000
 init(port)
