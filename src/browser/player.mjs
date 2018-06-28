0e4a484dba3876f01526a62726f3666d0360befa
Feross Aboukhadijeh <feross@feross.org>
2018-06-28 12:01:06 -0700

    only init player once, lazily

--- src/browser/player.mjs
+++ src/browser/player.mjs
@@ -2,11 +2,17 @@ import Timidity from 'timidity'
 
 let player
 
-export function load (url) {
+function initPlayer () {
+  if (player) return
   player = new Timidity('/timidity')
+}
+
+export function load (url) {
+  initPlayer()
   player.load(url)
 }
 
 export function play () {
+  initPlayer()
   player.play()
 }
