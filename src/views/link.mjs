829ed015694d4d4f5d5db543701237cd9fb763d6
Feross Aboukhadijeh <feross@feross.org>
2018-07-09 22:18:49 -0700

    link changes

--- src/views/link.mjs
+++ src/views/link.mjs
@@ -1,32 +1,43 @@
-import { h } from 'preact' /** @jsx h */
+import { Component, h } from 'preact' /** @jsx h */
 import c from 'classnames'
 
-const Link = (props, context) => {
-  const {
-    color = context.theme.mainColor,
-    class: className,
-    href,
-    onClick,
-    external = false, /* router should not capture click */
-    newtab = false, /* open link in new tab */
-    underlineHover = true,
-    ...rest
-  } = props
+export default class Link extends Component {
+  render (props) {
+    const { theme } = this.context
+    const {
+      color = theme.mainColor,
+      children,
+      class: className,
+      href,
+      onClick,
+      external = false, /* router should not capture click */
+      newtab = false, /* open link in new tab */
+      underlineHover = true,
+      ...rest
+    } = props
 
-  const cls = underlineHover ? 'underline-hover' : ''
+    const cls = underlineHover ? 'underline-hover' : ''
 
-  return (
-    <a
-      class={c('pointer no-underline', color, cls, className)}
-      href={href}
-      onClick={onClick}
-      rel={c(external && 'external', newtab && 'noopener') || null}
-      target={newtab ? '_blank' : null}
-      {...rest}
-    >
-      {children}
-    </a>
-  )
+    return (
+      <a
+        class={c('pointer no-underline', color, cls, className)}
+        href={href}
+        onClick={onClick && this.onClick}
+        rel={c(external && 'external', newtab && 'noopener') || null}
+        target={newtab ? '_blank' : null}
+        {...rest}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  onClick = e => {
+    const { onClick } = this.props
+    if (onClick) {
+      e.stopPropagation()
+      e.preventDefault()
+      onClick(e)
+    }
+  }
 }
-
-export default Link
