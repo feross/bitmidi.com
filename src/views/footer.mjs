b825eaedbcb5c51a5fbbfc61d91472f61839d2b9
Feross Aboukhadijeh <feross@feross.org>
2018-07-14 20:47:39 -0700

    Decompose HorizListItem and HorizListDivider out

--- src/views/footer.mjs
+++ src/views/footer.mjs
@@ -1,33 +1,26 @@
 import { h } from 'preact' /** @jsx h */
 
 import Link from './link'
+import { HorizListItem, HorizListDivider } from './horiz-list'
 
 const Footer = (props, context) => {
   return (
     <footer class='f6 lh-copy silver w-100 mt5 mb4 tc'>
-      <FooterItem>
+      <HorizListItem>
         Built by <Link href='https://twitter.com/feross' newtab>@feross</Link>
-      </FooterItem>
-      <FooterDivider />
+      </HorizListItem>
+      <HorizListDivider />
 
-      <FooterItem>
+      <HorizListItem>
         Thanks to <Link href='https://feross.org/thanks/' newtab>my supporters</Link>
-      </FooterItem>
-      <FooterDivider />
+      </HorizListItem>
+      <HorizListDivider />
 
-      <FooterItem>
+      <HorizListItem>
         Powered by <Link href='https://github.com/feross/bitmidi.com' newtab>open source</Link>
-      </FooterItem>
+      </HorizListItem>
     </footer>
   )
-}
-
-const FooterItem = ({ children }) => {
-  return <div class='dib nowrap mh2'>{children}</div>
-}
-
-const FooterDivider = () => {
-  return <span class='mh1'>•</span>
 }
 
 export default Footer
---
+++ src/views/horiz-list.mjs
@@ -0,0 +1,9 @@
+import { h } from 'preact' /** @jsx h */
+
+export const HorizListItem = ({ children }) => {
+  return <div class='dib nowrap'>{children}</div>
+}
+
+export const HorizListDivider = () => {
+  return <span class='mh2 ph1'>•</span>
+}
