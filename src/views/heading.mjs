d3cbf9eeecb8fa5c205c8fefb34ac937f44adb4f
Feross Aboukhadijeh <feross@feross.org>
2018-07-20 11:50:08 -0700

    heading: add level prop

--- src/views/heading.mjs
+++ src/views/heading.mjs
@@ -2,12 +2,19 @@ import { h } from 'preact' /** @jsx h */
 import c from 'classnames'
 
 const Heading = props => {
-  const { children, class: className, ...rest } = props
+  const {
+    children,
+    class: className,
+    level = 1,
+    ...rest
+  } = props
+
+  const HeadingElement = `h${level}`
 
   return (
-    <h1 class={c('mv2 f3', className)} {...rest}>
+    <HeadingElement class={c('mv2 f3', className)} {...rest}>
       {children}
-    </h1>
+    </HeadingElement>
   )
 }
 
