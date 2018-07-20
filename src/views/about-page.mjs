49fa1aa7686e8ba9ac64b4cd3bb19bd843c201fd
Feross Aboukhadijeh <feross@feross.org>
2018-07-20 13:51:51 -0700

    add About Page stub

--- src/views/about-page.mjs
+++ src/views/about-page.mjs
@@ -1,23 +1,41 @@
 import { h } from 'preact' /** @jsx h */
 
 import config from '../../config'
+import { doMidiAll } from '../actions/midi'
 
+import Loader from './loader'
 import Heading from './heading'
 import PageComponent from './page-component'
 
 export default class AboutPage extends PageComponent {
-  load () {
+  async load () {
     const { dispatch } = this.context
+
     dispatch('APP_META', {
       title: `About ${config.title}`
     })
+
+    await dispatch(doMidiAll({ pageSize: 1 }))
   }
 
   render (props) {
+    if (!this.loaded) return <Loader center />
+
+    const { views } = this.context.store
+
     return (
       <div>
-        <Heading>About {config.title}, the free MIDI site</Heading>
-        <p>Hi</p>
+        <Heading>About {config.title}</Heading>
+        <p>Serving {views.all.total.toLocaleString()} MIDI files curated by volunteers around the world.</p>
+
+        <Heading>What is this site?</Heading>
+        <p>
+          I found a ZIP file with 100K+ MIDI files and tried to play it back, but was was disappointed to discover that most software doesn't playing back MIDI files anymore. :disappointed: Browsers have removed native support, QuickTime stopped supporting it, even the venerable VLC can't play it back. There were some websites that offered playback and a library of files to choose from, but they were missing many instruments or they were flash-based and not long for this world. So, I compiled the best MIDI player written in C to WebAssembly and put a frontend on it, so it's easy to browse. That's BitMidi.
+        </p>
+
+        <Heading>What are MIDI files?</Heading>
+        <p>TODO</p>
+
       </div>
     )
   }
