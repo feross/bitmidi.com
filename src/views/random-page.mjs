a383f44a1fe15f8ff890800a2f5d9edd550f0495
Feross Aboukhadijeh <feross@feross.org>
2018-07-20 11:47:52 -0700

    style

--- src/views/random-page.mjs
+++ src/views/random-page.mjs
@@ -8,10 +8,10 @@ import PageComponent from './page-component'
 export default class RandomPage extends PageComponent {
   load () {
     const { dispatch } = this.context
+    dispatch(doGoMidiRandom())
     dispatch('APP_META', {
       title: 'Random MIDI'
     })
-    dispatch(doGoMidiRandom())
   }
   render (props) {
     return <Loader center show label='Finding random MIDI' />
