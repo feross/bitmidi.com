e190a60912c37777ba03803d0c4a2dc88d081564
Feross Aboukhadijeh <feross@feross.org>
2018-07-20 13:47:19 -0700

    Simplify loader component

--- src/views/loader.mjs
+++ src/views/loader.mjs
@@ -7,47 +7,37 @@ import Image from './image'
 const Loader = props => {
   let {
     center,
-    children,
     class: className,
-    show = false,
     label,
-    style = {},
+    style,
     ...rest
   } = props
 
-  // Show loader
-  if (show) {
-    if (center) {
-      style.marginTop = 'calc(40vh - 80px)'
-      style.marginBottom = 'calc(40vh - 80px)'
+  if (center) {
+    style = {
+      marginTop: 'calc(40vh - 80px)',
+      marginBottom: 'calc(40vh - 80px)',
+      ...style
     }
-
-    return (
-      <div
-        class={c('tc animate-fade-in animate--delay animate--fast', className)}
-        style={style}
-        {...rest}
-      >
-        { label &&
-          <Heading
-            class='animate-pulse animate--normal animate--infinite'
-          >
-            {label}…
-          </Heading>
-        }
-        <Image src='/img/icon-loader.svg' alt='Loading…' />
-      </div>
-    )
   }
 
-  // Show content
   return (
     <div
-      class={className}
+      class={c(
+        'tc animate-fade-in animate--delay animate--fast',
+        className
+      )}
       style={style}
       {...rest}
     >
-      {children}
+      { label &&
+        <Heading
+          class='animate-pulse animate--normal animate--infinite'
+        >
+          {label}…
+        </Heading>
+      }
+      <Image src='/img/icon-loader.svg' alt='Loading…' />
     </div>
   )
 }
