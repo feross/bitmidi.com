ae78c41539efdd9e07b2382b83e4035eeb488b17
Feross Aboukhadijeh <feross@feross.org>
2018-07-21 01:04:50 -0700

    style

--- src/views/icon.mjs
+++ src/views/icon.mjs
@@ -3,12 +3,18 @@ import { h } from 'preact' /** @jsx h */
 import c from 'classnames'
 
 const Icon = props => {
-  const { name, class: className, size, ...rest } = props
+  const {
+    name,
+    class: className,
+    size = 24,
+    ...rest
+  } = props
+
   return (
     <i
       class={c('material-icons', className)}
       style={{
-        fontSize: size || 24
+        fontSize: size
       }}
       {...rest}
     >
