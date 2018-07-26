b7df268d8c24c2b7baa93ab6686d92cbd3be328d
Feross Aboukhadijeh <feross@feross.org>
2018-07-25 17:21:09 -0700

    Fix OpenGraph hero image URL

--- src/config.mjs
+++ src/config.mjs
@@ -22,7 +22,7 @@ export const description = oneLine`
 export const twitterUser = 'BitMidi'
 
 // Default image to represent the site in social sharing
-export const siteImage = '/img/hero.png'
+export const siteImage = '/img/hero.jpg'
 
 // Google Analytics tracking ID
 export const analyticsId = 'UA-3898076-25'
