c8339194581303a3b181c7171a0e7c6c0c7d9919
Feross Aboukhadijeh <feross@feross.org>
2018-08-14 11:07:32 -0700

    Move share-twitter.mjs to server/

--- src/server/schedule.mjs
+++ src/server/schedule.mjs
@@ -1,6 +1,6 @@
 import schedule from 'node-schedule'
 
-import shareTwitter from '../lib/share-twitter'
+import shareTwitter from './share-twitter'
 import { isProd } from '../config'
 
 export default function init () {
