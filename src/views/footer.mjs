aae5c9c570266dab1d86572d50ab68bc3b0039b6
Feross Aboukhadijeh <feross@feross.org>
2018-08-14 21:15:33 -0700

    Add random tips to footer

--- src/views/footer.mjs
+++ src/views/footer.mjs
@@ -1,7 +1,22 @@
 import { h } from 'preact' /** @jsx h */
 
+import { siteTwitter } from '../config'
+
 import Link from './link'
 import { HorizListItem, HorizListDivider } from './horiz-list'
+
+const TIPS = [
+  <span>You can drag-and-drop a MIDI file onto this page to play it!</span>,
+  <span>
+    This project is open source on{' '}
+    <Link href='https://github.com/feross/bitmidi.com' newtab>GitHub</Link>!
+  </span>,
+  <span>
+    We tweet a new MIDI every day.{' '}
+    <Link src={`https://twitter.com/${siteTwitter}`}>Follow @{siteTwitter}</Link>{' '}
+    on Twitter!
+  </span>
+]
 
 const Footer = (props, context) => {
   const { app } = context.store
@@ -10,33 +25,47 @@ const Footer = (props, context) => {
   if (app.pending > 0) return null
 
   return (
-    <footer class='f6 lh-copy mid-gray mw7 center mt4 mb4 tc'>
-      <HorizListItem>
-        <Link href='/about'>
-          About BitMidi
-        </Link>
-      </HorizListItem>
+    <footer class='f6 lh-copy mid-gray mw7 center mv4 tc'>
+      <div class='mv3'>
+        <RandomTip />
+      </div>
+      <div class='mv3'>
+        <HorizListItem>
+          <Link href='/about'>
+            About BitMidi
+          </Link>
+        </HorizListItem>
 
-      <HorizListDivider />
+        <HorizListDivider />
 
-      <HorizListItem>
-        <Link href='/random'>Random MIDI</Link>
-      </HorizListItem>
+        <HorizListItem>
+          <Link href='/random'>Random MIDI</Link>
+        </HorizListItem>
 
-      <HorizListDivider />
+        <HorizListDivider />
 
-      <HorizListItem>
-        <Link href='https://twitter.com/bitmidi' newtab>
-          @BitMidi
-        </Link>
-      </HorizListItem>
+        <HorizListItem>
+          <Link href={`https://twitter.com/${siteTwitter}`} newtab>
+            @BitMidi
+          </Link>
+        </HorizListItem>
 
-      <HorizListDivider />
+        <HorizListDivider />
 
-      <HorizListItem>
-        Built by Feross
-      </HorizListItem>
+        <HorizListItem>
+          Built by Feross
+        </HorizListItem>
+      </div>
     </footer>
+  )
+}
+
+const RandomTip = () => {
+  const tip = TIPS[Math.floor(TIPS.length * Math.random())]
+  return (
+    <span>
+      <strong>Pro Tip</strong>: {tip}
+    </span>
   )
 }
 
