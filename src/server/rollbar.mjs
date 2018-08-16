8f682b72395f5052e2cc5de7d61989af5c300529
Feross Aboukhadijeh <feross@feross.org>
2018-08-15 17:57:34 -0700

    rollbar: ignore 404 errors

--- src/server/rollbar.mjs
+++ src/server/rollbar.mjs
@@ -6,6 +6,11 @@ if (isProd) {
   global.rollbar = new Rollbar({
     accessToken: rollbarSecret.accessToken,
     captureUncaught: true,
-    captureUnhandledRejections: true
+    captureUnhandledRejections: true,
+    checkIgnore: (isUncaught, args) => {
+      // Ignore 404 errors
+      const err = args[0]
+      return !isUncaught && err && err.status === 404
+    }
   })
 }
