551559d430ae3cc1f970a7213d33b21df965a8a3
Felix Wu <mail@flxwu.com>
2018-08-28 23:34:14 +0200

    perform a whole setup + build on travis

---
+++ .travis/db-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# replace the user with the Travis root user
+sed -i -e "s/.*user.*/user: 'root',/" secret/index-sample.mjs
+cp secret/index-sample.mjs secret/index.mjs
+
+# database migrations
+npm run knex -- migrate:latest
+
+# get sample midis (13Kb)
+curl http://www.jsbach.net/midi/sankey/802-805.zip --output sample.zip
+unzip sample.zip -d midi-samples
+
+# import the samples
+node -r @babel/register tools/import.mjs midi-samples/
