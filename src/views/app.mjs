be4ef4ceecef6ef6bc9c5c80dd7616bf01b74221
Feross Aboukhadijeh <feross@feross.org>
2019-01-06 08:51:56 -0800

    Break all long lines
    Resolves Search Console issue

--- src/views/app.mjs
+++ src/views/app.mjs
@@ -18,7 +18,7 @@ export default class App extends Component {
       <div id='root'>
         <Title title={app.title} />
         <Header />
-        <main class='mt4 mb5 ph3 ph3-safe mw7 center'>
+        <main class='mt4 mb5 ph3 ph3-safe mw7 center break-word'>
           <Page url={location.url} isServerRendered={app.isServerRendered} />
           <div class='tc light-gray'>
             {errors.map(err => <div><small>{err.message}</small></div>)}
