28c0a150a85520ed937bdbe45e2f1fba97763fe9
Feross Aboukhadijeh <feross@feross.org>
2019-05-27 14:12:20 -0700

    Minify CSS (saves 1KB before gzip)

--- tools/build-css.mjs
+++ tools/build-css.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
+import cssnano from 'cssnano'
 import minimist from 'minimist'
 import oneLine from 'common-tags/lib/oneLine'
+import postcss from 'postcss'
 import PurgeCss from 'purgecss'
 import rimraf from 'rimraf'
 import { join, basename, dirname } from 'path'
@@ -9,69 +11,93 @@ import { writeFileSync } from 'fs'
 
 import { rootPath, theme } from '../src/config'
 
-const startTime = Date.now()
+const outputPath = join(rootPath, './static/bundle.css')
+
+const shortOutputPath = join(
+  basename(dirname(outputPath)),
+  basename(outputPath)
+)
 
 const argv = minimist(process.argv.slice(2), {
   boolean: ['debug', 'verbose']
 })
 
-const outputPath = join(rootPath, './static/bundle.css')
+run()
 
-rimraf.sync(outputPath)
+async function run () {
+  const startTime = Date.now()
 
-// CSS files to concat, purge, and minify
-const css = [
-  './node_modules/tachyons/css/tachyons.min.css',
-  './src/server/index.css'
-]
+  // Remove existing output CSS file
+  rimraf.sync(outputPath)
 
-// Files to search for selectors within
-const content = [
-  './src/views/*.mjs',
-  './src/server/*.ejs'
-]
+  // CSS files to purge, concat, and minify
+  const css = [
+    './node_modules/tachyons/css/tachyons.min.css',
+    './src/server/index.css'
+  ]
 
-// Selectors to never remove
-const whitelistPatternsChildren = [
-  // Theme colors
-  ...Object.values(theme).map(color => new RegExp(`.*?${color}.*?`))
-]
+  const files = purge(css)
 
-const purgeCss = new PurgeCss({
-  css,
-  content,
-  whitelistPatternsChildren,
-  keyframes: true,
-  rejected: argv.debug
-})
+  // Concat all CSS files
+  let bundle = files.reduce((acc, file) => {
+    return acc + file.css
+  }, '')
 
-const files = purgeCss.purge()
+  bundle = await minify(bundle)
 
-const bundle = files.reduce((acc, file) => {
-  return acc + file.css
-}, '')
-
-// Write file
-writeFileSync(outputPath, bundle)
-
-if (argv.debug) {
-  files.forEach(file => {
-    console.log(`Removed from ${file.file}:`)
-    file.rejected.forEach(rejectedDecl => console.log(`  ${rejectedDecl}`))
-  })
-}
-
-if (argv.verbose) {
-  const shortOutputPath = join(
-    basename(dirname(outputPath)),
-    basename(outputPath)
-  )
-  const buildTime = ((Date.now() - startTime) / 1000).toFixed(2)
-  const currentTime = new Date().toLocaleTimeString()
+  // Write file
+  writeFileSync(outputPath, bundle)
 
   // Print success message
-  console.log(oneLine`
-    ${bundle.length} bytes written to ${shortOutputPath}
-    (${buildTime} seconds) at ${currentTime}
-  `)
+  if (argv.verbose) {
+    const buildTime = ((Date.now() - startTime) / 1000).toFixed(2)
+    const currentTime = new Date().toLocaleTimeString()
+
+    console.log(oneLine`
+      ${bundle.length} bytes written to ${shortOutputPath}
+      (${buildTime} seconds) at ${currentTime}
+    `)
+  }
+}
+
+/**
+ * Removes unused CSS declarations from CSS files
+ */
+function purge (css) {
+  // Files to search for used selectors. Used selectors will not be purged.
+  const content = [
+    './src/views/*.mjs',
+    './src/server/*.ejs'
+  ]
+
+  // Whitelist of selectors to never purge
+  const whitelistPatternsChildren = [
+    // Theme colors
+    ...Object.values(theme).map(color => new RegExp(`.*?${color}.*?`))
+  ]
+
+  const purgeCss = new PurgeCss({
+    css,
+    content,
+    whitelistPatternsChildren,
+    keyframes: true,
+    rejected: argv.debug
+  })
+
+  const files = purgeCss.purge()
+
+  // Print removed CSS selectors
+  if (argv.debug) {
+    files.forEach(file => {
+      console.log(`Removed from ${file.file}:`)
+      file.rejected.forEach(rejectedDecl => console.log(`  ${rejectedDecl}`))
+    })
+  }
+
+  return files
+}
+
+async function minify (css) {
+  const result = await postcss([cssnano]).process(css, { to: outputPath })
+  return result.css
 }
