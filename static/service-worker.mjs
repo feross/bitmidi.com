6df2782c539728251ff4e9aab1a31302fc798c48
Feross Aboukhadijeh <feross@feross.org>
2019-06-20 21:02:28 -0700

    Simplify service worker, drop code for old Chrome

--- static/service-worker.mjs
+++ static/service-worker.mjs
@@ -2,87 +2,69 @@
 
 'use strict'
 
-// Incrementing CACHE_VERSION will kick off the 'install' event and force previously
-// cached resources to be cached again.
+// Incrementing CACHE_VERSION will kick off the 'install' event and force
+// previously cached resources to be cached again.
 const CACHE_VERSION = 1
 
-const CURRENT_CACHES = {
+const CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 }
 const START_URL = '/'
 const OFFLINE_URL = './offline.html'
 
-function createCacheBustingRequest (url) {
-  const request = new Request(url, { cache: 'reload' })
-
-  // NOTE: `request.cache` is not supported in Chrome, so check if the option had
-  // any effect.
-  if ('cache' in request) {
-    return request
-  }
-
-  // If `request.cache` is not supported, manually append a cache-busting param
-  const bustedUrl = new URL(url, self.location.href)
-  bustedUrl.search += (bustedUrl.search ? '&' : '') + 'c=' + Date.now()
-  return new Request(bustedUrl)
-}
-
-async function addToCache (url) {
-  // We can't use cache.add() here, since we want `url` to be the cache key but
-  // the actual URL we end up requesting might include a cache-busting parameter.
-  const response = await fetch(createCacheBustingRequest(url))
-  if (!response.ok) {
-    throw new Error(`HTTP response error. ${response.status}`)
-  }
-  const cache = await caches.open(CURRENT_CACHES.offline)
-  await cache.put(url, response)
+async function cacheAdd (cache, url) {
+  // Set cache mode to 'reload' to ensure response comes from the network
+  return cache.add(new Request(url, { cache: 'reload' }))
 }
 
 self.addEventListener('install', event => {
-  event.waitUntil(Promise.all([
-    // Note: START_URL is never actually served from cache by the service worker. This
-    // is a temporary hack to pass the audit in Lighthouse.
-    addToCache(START_URL),
-    addToCache(OFFLINE_URL)
-  ]))
+  event.waitUntil(async function () {
+    const offlineCache = await caches.open(CACHES.offline)
+    return Promise.all([
+      // Note: START_URL is never actually served from cache by the service
+      // worker. This is a temporary hack to pass the audit in Lighthouse.
+      cacheAdd(offlineCache, START_URL),
+      cacheAdd(offlineCache, OFFLINE_URL)
+    ])
+  }())
 })
 
 self.addEventListener('activate', event => {
-  // Delete all caches that aren't named in CURRENT_CACHES. While there is only one
-  // cache in this example, the same logic will handle the case where there are
-  // multiple versioned caches.
-  const expectedNames = Object.values(CURRENT_CACHES)
+  event.waitUntil(async function () {
 
-  event.waitUntil((async () => {
+    // Delete all caches that aren't named in CACHES
+    const expectedNames = new Set(Object.values(CACHES))
     const names = await caches.keys()
-    await Promise.all(names.map(name => {
-      if (!expectedNames.includes(name)) {
-        console.log('Deleting out-of-date cache', name)
-        return caches.delete(name)
-      }
-    }))
-  })())
+
+    return Promise.all(
+      names
+        .filter(name => !expectedNames.has(name))
+        .map(name => {
+          console.log('Deleting out-of-date cache', name)
+          return caches.delete(name)
+        })
+    )
+  }())
 })
 
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    // Only handle navigation requests (top-level HTML pages)
-    event.respondWith(
-      fetch(event.request).catch(error => {
-        // The catch is only triggered if fetch() throws an exception, which will
-        // most likely happen due to the server being unreachable. If fetch() returns
-        // a valid HTTP response with an response code in the 4xx or 5xx range, the
-        // catch() will NOT be called.
-        const { url } = event.request
-        console.log(`Fetch of ${url} failed, returning offline page.`, error)
-        return caches.match(OFFLINE_URL)
-      })
-    )
+  if (event.request.mode !== 'navigate') {
+    // Only intercept navigation requests (i.e. top-level HTML pages). If
+    // neither this fetch handler nor any others call event.respondWith(), the
+    // request will be handled as if there were no service worker.
+    return
   }
 
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there were
-  // no service worker involvement.
+  event.respondWith(async function () {
+    try {
+      await fetch(event.request)
+    } catch (err) {
+      // fetch() throws an exception when the server is unreachable. If fetch()
+      // returns a valid HTTP response with a status code in the 4xx or 5xx
+      // range, then an exception will NOT be thrown.
+      const { url } = event.request
+      console.log(`Return offline page because fetch failed for ${url}: `, err)
+      return caches.match(OFFLINE_URL)
+    }
+  }())
 })
