1ca28882318beda171b79d0217b7e3bd7fcbc542
Feross Aboukhadijeh <feross@feross.org>
2019-06-22 12:19:34 -0700

    Resize low quality images to max width of 960

--- src/lib/serve-webp.mjs
+++ src/lib/serve-webp.mjs
@@ -1,6 +1,9 @@
 // TODO: publish to npm
 
+import Debug from 'debug'
+import ImageminGm from 'imagemin-gm'
 import ImageminWebp from 'imagemin-webp'
+import imageSize from 'image-size'
 import mkdirp from 'mkdirp'
 import parseUrl from 'parseurl'
 import send from 'send'
@@ -9,6 +12,8 @@ import { dirname, extname, join, normalize, relative, resolve, sep } from 'path'
 import { promisify } from 'util'
 import { randomBytes } from 'crypto'
 import { tmpdir } from 'os'
+
+const debug = Debug('bitmidi:serve-webp')
 
 const accessAsync = promisify(access)
 const mkdirpAsync = promisify(mkdirp)
@@ -27,6 +32,10 @@ const convertWebpLow = ImageminWebp({
   quality: 40
 })
 
+const resizeLow = (new ImageminGm()).resize({
+  width: 960
+})
+
 export default function serveWebp (root, opts = {}) {
   if (typeof root !== 'string') {
     throw new Error('Option `root` must be a string')
@@ -36,6 +45,7 @@ export default function serveWebp (root, opts = {}) {
 
   // use a cache folder in /tmp, since that's guaranteed to be writable
   const cacheRoot = join(tmpdir(), randomBytes(16).toString('hex'))
+  debug('Cache root: %s', cacheRoot)
 
   return async (req, res, next) => {
     // decode the path
@@ -111,13 +121,19 @@ export default function serveWebp (root, opts = {}) {
       .pipe(res)
 
     async function convertToWebp () {
-      const file = await readFileAsync(path)
-      const outputFile = isLow
-        ? await convertWebpLow(file)
-        : await convertWebp(file)
+      let file = await readFileAsync(path)
+      if (isLow) {
+        const { width } = imageSize(file)
+        if (width > 960) {
+          file = await resizeLow(file)
+        }
+        file = await convertWebpLow(file)
+      } else {
+        file = await convertWebp(file)
+      }
       const outputPath = join(cacheRoot, relativeWebpPath)
       await mkdirpAsync(dirname(outputPath))
-      await writeFileAsync(outputPath, outputFile)
+      await writeFileAsync(outputPath, file)
     }
   }
 }
