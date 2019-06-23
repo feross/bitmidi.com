599db4f8765089f9b3e3fca36b37907b6b767313
Feross Aboukhadijeh <feross@feross.org>
2019-06-23 15:58:16 -0700

    add static variables to pages to show/hide header/footer

--- src/views/app.mjs
+++ src/views/app.mjs
@@ -17,14 +17,14 @@ export default class App extends Component {
     return (
       <div id='root'>
         <Title title={app.title} />
-        <Header />
+        { Page.showHeader && <Header /> }
         <main class='mt4 mb5 ph3 ph3-safe mw7 center break-word'>
           <Page url={location.url} isServerRendered={app.isServerRendered} />
           <div class='tc light-gray'>
             {errors.map(err => <div><small>{err.message}</small></div>)}
           </div>
         </main>
-        <Footer />
+        { Page.showFooter && <Footer /> }
       </div>
     )
   }
--- src/views/page.mjs
+++ src/views/page.mjs
@@ -1,6 +1,9 @@
 import { Component } from 'preact'
 
 export default class Page extends Component {
+  static showHeader = true
+  static showFooter = true
+
   constructor () {
     super()
     this.state = {
