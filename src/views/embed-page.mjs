32fe2430fe22f78d272e91cf6fab8ea43ced6f26
Feross Aboukhadijeh <feross@feross.org>
2019-06-23 18:50:46 -0700

    twitter embed: test fix for safari

--- src/views/embed-page.mjs
+++ src/views/embed-page.mjs
@@ -59,7 +59,12 @@ export default class EmbedPage extends Page {
 
     const midi = data.midis[midiSlug]
 
-    return <EmbedMidi midi={midi} />
+    return (
+      <div>
+        <EmbedMidi midi={midi} />
+        <audio src='data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV' preload='auto' autoplay />
+      </div>
+    )
   }
 }
 
