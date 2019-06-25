c61489f51f9a683e39476469cfc7743008562dba
Feross Aboukhadijeh <feross@feross.org>
2019-06-24 18:38:53 -0700

    adsense: go full client-side react component approach

--- src/views/adsense.mjs
+++ src/views/adsense.mjs
@@ -3,6 +3,36 @@ import { Component, h } from 'preact' /** @jsx h */
 import { isBrowser, tokens } from '../config'
 
 export default class Adsense extends Component {
+  componentDidMount () {
+    console.log(`Adsense componentDidMount, isBrowser: ${isBrowser}`)
+    if (isBrowser) {
+      console.log('ad push')
+      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+  }
+
+  shouldComponentUpdate () {
+    console.log(`Adsense shouldComponentUpdate, isBrowser: ${isBrowser}`)
+    return false
+  }
+
+  render (props, _, { store }) {
+    console.log(`Adsense render, isBrowser: ${isBrowser}`)
+    return (
+      <div {...props}>
+        <ins
+          key={Math.random()}
+          class='adsbygoogle'
+          style={{ display: 'block' }}
+          data-ad-client={tokens.adsense}
+          data-ad-slot='2581488270'
+          data-ad-format='auto'
+          data-full-width-responsive='true'
+        />
+      </div>
+    )
+  }
+
   // componentDidMount () {
   //   const { store } = this.context
   //   if (store.app.isServerRendered) return
@@ -19,29 +49,34 @@ export default class Adsense extends Component {
   //   return true
   // }
 
-  render (props, _, { store }) {
-    console.log(`Adsense render, isBrowser: ${isBrowser}`)
+  // shouldComponentUpdate () {
+  //   console.log(`Adsense shouldComponentUpdate, isBrowser: ${isBrowser}`)
+  //   return false
+  // }
 
-    if (isBrowser) {
-      return <div dangerouslySetInnerHTML={{ __html: '' }} />
-    }
+  // render (props, _, { store }) {
+  //   console.log(`Adsense render, isBrowser: ${isBrowser}`)
 
-    return (
-      <div {...props}>
-        <ins
-          class='adsbygoogle'
-          style={{ display: 'block' }}
-          data-ad-client={tokens.adsense}
-          data-ad-slot='2581488270'
-          data-ad-format='auto'
-          data-full-width-responsive='true'
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: 'console.log("ad push"); (adsbygoogle = window.adsbygoogle || []).push({})'
-          }}
-        />
-      </div>
-    )
-  }
+  //   if (isBrowser) {
+  //     return <div dangerouslySetInnerHTML={{ __html: '' }} />
+  //   }
+
+  //   return (
+  //     <div {...props}>
+  //       <ins
+  //         class='adsbygoogle'
+  //         style={{ display: 'block' }}
+  //         data-ad-client={tokens.adsense}
+  //         data-ad-slot='2581488270'
+  //         data-ad-format='auto'
+  //         data-full-width-responsive='true'
+  //       />
+  //       <script
+  //         dangerouslySetInnerHTML={{
+  //           __html: 'console.log("ad push"); (adsbygoogle = window.adsbygoogle || []).push({})'
+  //         }}
+  //       />
+  //     </div>
+  //   )
+  // }
 }
