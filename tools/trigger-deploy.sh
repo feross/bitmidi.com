829a99f1a34d2b44faa9d770ea083f043d299b0a
Feross Aboukhadijeh <feross@feross.org>
2019-10-23 18:26:36 -0700

    add trigger deploy script

---
+++ tools/trigger-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Trigger a deploy on server
+set -e
+
+figlet "Deploying..."
+git push
+ssh future -t zsh -ci "/home/feross/www/bitmidi.com/tools/deploy.sh"
+curl https://api.rollbar.com/api/1/deploy/ -F access_token=$(node -r @babel/register -p 'require("./secret").rollbar.accessToken') -F environment=production -F revision=$(git log -n 1 --pretty=format:"%H") -F local_username=$(whoami)
+figlet "Deployed"
