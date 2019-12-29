8ceaaede10ea88c5428be029a958631778729b68
Feross Aboukhadijeh <feross@feross.org>
2019-12-28 19:15:51 -0800

    Post to reddit on correct schedule again

--- src/server/schedule.js
+++ src/server/schedule.js
@@ -5,7 +5,7 @@ import shareTwitter from './share-twitter'
 import { isProd } from '../config'
 
 const DAILY_12AM = '0 0 * * *'
-const DAILY_5PM = '6 19 * * *' // TEMP TEST
+const DAILY_5PM = '0 17 * * *'
 
 export default function init (port) {
   const isMainProcess = port % 10 === 0
