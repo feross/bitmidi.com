78555985829ab67c2be0867b86b0cb93590adc91
Feross Aboukhadijeh <feross@feross.org>
2019-12-30 00:11:50 -0800

    fix Random so it doesn't re-randomize after picking one

--- src/views/random.js
+++ src/views/random.js
@@ -1,10 +1,15 @@
 import { toChildArray } from 'preact'
+import { useState } from 'preact/hooks'
 
 const ROTATE_INTERVAL = 10 * 60 * 1000 // 10 minutes
 
 const Random = ({ children }) => {
   children = toChildArray(children)
-  const index = Math.floor(Date.now() / ROTATE_INTERVAL) % children.length
+
+  const [index] = useState(
+    Math.floor(Date.now() / ROTATE_INTERVAL) % children.length
+  )
+
   return children[index]
 }
 
