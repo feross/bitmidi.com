bebd3f7f23e1aa290e82f621f9be849e8a970c0f
Feross Aboukhadijeh <feross@feross.org>
2020-05-26 17:59:32 -0700

    ads

--- src/server/router-ads.js
+++ src/server/router-ads.js
@@ -8,24 +8,24 @@ import * as config from '../config'
 const { rootPath } = config
 
 let adsTxtOptimize = ''
-let adsTxtNewor = ''
 let adsTxtAdsense = ''
+// let adsTxtNewor = ''
 
 function updateAdsTxt () {
+  readFile(join(rootPath, 'static', 'ads-adsense.txt'), (err, file) => {
+    if (err) throw err
+    adsTxtAdsense = file
+  })
+
   get.concat('https://cdn4.buysellads.net/ads.txt', (err, _, body) => {
     if (err) throw err
     adsTxtOptimize = body.toString()
   })
 
-  readFile(join(rootPath, 'static', 'ads-newor.txt'), (err, file) => {
-    if (err) throw err
-    adsTxtNewor = file
-  })
-
-  readFile(join(rootPath, 'static', 'ads-adsense.txt'), (err, file) => {
-    if (err) throw err
-    adsTxtAdsense = file
-  })
+  // readFile(join(rootPath, 'static', 'ads-newor.txt'), (err, file) => {
+  //   if (err) throw err
+  //   adsTxtNewor = file
+  // })
 }
 
 updateAdsTxt()
@@ -35,7 +35,11 @@ const router = Router()
 
 // Serve ads.txt redirect
 router.get('/ads.txt', (req, res) => {
-  const body = adsTxtOptimize + '\n' + adsTxtNewor + '\n' + adsTxtAdsense
+  const body = [
+    adsTxtAdsense,
+    adsTxtOptimize
+    // adsTxtNewor
+  ].join('\n')
   res.setHeader('content-type', 'text/plain')
   res.send(body)
 })
