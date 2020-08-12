715df8e277a4173bcd60eaf70035f9f0ea26f678
Feross Aboukhadijeh <feross@feross.org>
2020-08-12 12:02:27 -0700

    reduce memo size by half

--- src/lib/memo.js
+++ src/lib/memo.js
@@ -2,7 +2,7 @@ import pMemoize from 'p-memoize'
 import QuickLRU from 'quick-lru'
 
 const MEMO_MAX_AGE = 24 * 60 * 60 * 1000 // 24 hours
-const MEMO_MAX_SIZE = 50 * 1000
+const MEMO_MAX_SIZE = 25 * 1000
 
 export function memo (fn) {
   return pMemoize(fn, {
